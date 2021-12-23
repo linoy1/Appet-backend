f81db2284151c1911771287359f9563f0083f245
linoy1 <linoysimantov@se.shenkar.ac.il>
2021-12-23 19:55:41 +0200

    backend upload2

--- test.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch');
-
-fetch('https://api.thedogapi.com/v1/breeds/search?q=akita')
-  .then((response) => response.json())
-  .then((data) => console.log(data[0].name))
-  .catch((err) => console.log(err));
